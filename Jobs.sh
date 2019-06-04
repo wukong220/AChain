43fff12d37b921f7fa02fe95a7cca8bb49bc5f8d
wukong220 <wusong220@outlook.com>
2019-06-04 21:51:15 +0800

    z-coordinate

--- Jobs.sh
+++ Jobs.sh
@@ -8,13 +8,20 @@
 #    cd ..
 #done 
 
-for t in 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020
+for i in 001 002 003 004 005 006 007 008
+do 
+ sed "59,184s/001/$t/g" test.in > $t.act.in 
+ bsub -J $t -n 1 mpirun lmp_wk -l $t.log -i $t.act.in > $t.data
+ echo "bsub -J $t -n 1 mpirun lmp_wk -l $t.log -i $t.act.in > $t.data"
+done 
+
+for t in 009 010 011 012 013 014 015 016 017 018 019 020 
 do
      sed "59,184s/001/$t/g" test.in > $t.act.in 
    # mpirun -np 4 lmp_wk -l $t.log -i $t.act.in
      #bsub -J $t -n 1 mpirun lmp_wk -l $t.log -i $t.act.in > $t.data
      #echo "bsub -J $t -n 1 mpirun lmp_wk -l $t.log -i $t.act.in > $t.data"
-     bsub  -q gchen -R "span[ptile=8]" -n 1 mpich2-mpiexec -np 1 lmp_wk"<"$t.act.in">"$t.data
-    echo "bsub  -q gchen -R "span[ptile=8]" -n 1 mpich2-mpiexec -np 1 lmp_wk"<"$t.act.in">"$t.data"
+     bsub  -q gchen -R "span[ptile=1]" -n 1 mpich2-mpiexec -np 1 lmp_wk"<"$t.act.in">"$t.data
+    echo "bsub  -q gchen -R "span[ptile=1]" -n 1 mpich2-mpiexec -np 1 lmp_wk"<"$t.act.in">"$t.data"
 done
 
